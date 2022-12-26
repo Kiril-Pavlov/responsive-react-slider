4c317a9b0af5caaf1692383da7930b6c4b22a42f
Kiril-Pavlov <Kire_P@yahoo.com>
2022-12-26 20:46:07 +0100

    made basic mapping for cards

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import Card from './components/Card';
+import Card from "./components/Card";
 
 function App() {
+  let sliders = [
+    {
+      number: "1",
+    },
+    {
+      number: "2",
+    },
+    {
+      number: "3",
+    },
+    {
+      number: "4",
+    },
+  ];
   return (
-    <div className="App">
-      <Card/>
+    <div className="flex items-center justify-center gap-5">
+      {sliders.map((card) => {
+        return <Card number={card.number}/>;
+      })}
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Card = () => {
+const Card = ({number}) => {
   return (
-    <div className='flex items-center justify-center w-16 h-16 bg-slate-400 mx-auto my-16'>
-        TEST
+    <div className='flex items-center justify-center w-64 h-64 bg-slate-400 my-16'>
+        <span className='font-bold text-6xl text-yellow-100'>{number}</span>
     </div>
   )
 }
