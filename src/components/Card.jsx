1b80254e330c64fa299aa133bc3ade6693f19d89
Kiril-Pavlov <Kire_P@yahoo.com>
2022-12-26 20:36:15 +0100

    created card component and setup tailwind

---
+++ src/components/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='flex items-center justify-center w-16 h-16 bg-slate-400 mx-auto my-16'>
+        TEST
+    </div>
+  )
+}
+
+export default Card
