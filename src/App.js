b2bc38d3027228315ad020d5df7e84962f7cc5cf
Kiril-Pavlov <Kire_P@yahoo.com>
2022-12-27 21:33:00 +0100

    basic slider logic done. need to fix bugs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
+import { useState } from "react";
 import Card from "./components/Card";
 
 function App() {
+  const [activeSlider,setActiveSlider] =useState(1)
+
   let sliders = [
     {
+      id:0,
+      number: "0",
+    },
+    {
+      id:1,
       number: "1",
     },
     {
+      id:2,
       number: "2",
     },
     {
+      id:3,
       number: "3",
     },
     {
+      id:4,
       number: "4",
     },
   ];
+
+  let modifiedSlides = sliders.slice(activeSlider,activeSlider+3)
+
+  const handleShiftLeft = () =>{
+    setActiveSlider(activeSlider-1)
+  }
+
+  const handleShiftRight = () =>{
+    setActiveSlider(activeSlider+1)
+  }
+
   return (
     <div className="flex items-center justify-center gap-5">
-      {sliders.map((card) => {
+      <button onClick={handleShiftLeft}>LEFT</button>
+      {modifiedSlides.map((card) => {
         return <Card number={card.number}/>;
       })}
+      <button onClick={handleShiftRight}>RIGHT</button>
     </div>
   );
 }
