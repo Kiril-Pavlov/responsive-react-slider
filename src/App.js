8dae1e787a924e5e762738e18790337b515e1f10
Kiril-Pavlov <Kire_P@yahoo.com>
2022-12-30 18:20:06 +0100

    Refactor to make Slider separate component

--- src/App.js
+++ src/App.js
@@ -1,104 +1,10 @@
-import { useEffect, useState } from "react";
-import Card from "./components/Card";
-
-import slider0 from "./assets/slider-0.jpg"
-import slider1 from "./assets/slider-1.webp"
-import slider2 from "./assets/slider-2.jpg"
-import slider3 from "./assets/slider-3.jpg"
-import slider4 from "./assets/slider-4.jpg"
-import slider5 from "./assets/slider-5.jfif"
-import slider6 from "./assets/slider-6.jpg"
-
-import {FaChevronLeft, FaChevronRight} from "react-icons/fa"
+import Slider from "./components/Slider";
 
 function App() {
-  const [activeSlider,setActiveSlider] =useState(0)
-  const [slidersNumber,setSlidersNumber] =useState(4)
-
-  useEffect(()=>{
-    handleScreenSizes()
-  },[])
-
-  const handleScreenSizes = ()=>{
-    if(window.innerWidth<500){
-      setSlidersNumber(1)
-    }else if(window.innerWidth<1000){
-      setSlidersNumber(2)
-    }else if(window.innerWidth<1500){
-      setSlidersNumber(3)
-    }else{
-      setSlidersNumber(4)
-    }
-  }
-
-
-
-  let sliders = [
-    {
-      id:0,
-      number: "0",
-      src: slider0,
-    },
-    {
-      id:1,
-      number: "1",
-      src: slider1,
-    },
-    {
-      id:2,
-      number: "2",
-      src: slider2,
-    },
-    {
-      id:3,
-      number: "3",
-      src: slider3,
-    },
-    {
-      id:4,
-      number: "4",
-      src: slider4,
-    },
-    {
-      id:5,
-      number: "5",
-      src: slider5,
-    },
-    {
-      id:6,
-      number: "6",
-      src:slider6,
-    },
-  ];
-
-  console.log(window.innerWidth)
-
-  let modifiedSlides = sliders.slice(activeSlider,activeSlider+slidersNumber)
-
-  const handleShiftLeft = () =>{
-    if(activeSlider===0){
-      setActiveSlider(0)
-    }else{
-      setActiveSlider(activeSlider-1)
-    }
-  }
-
-  const handleShiftRight = () =>{
-    if(activeSlider === sliders.length-slidersNumber){
-      setActiveSlider(activeSlider)
-    }else{
-      setActiveSlider(activeSlider+1)
-    }
-  }
+  
 
   return (
-    <div className="flex items-center justify-center gap-5">
-      <button onClick={handleShiftLeft}><FaChevronLeft size={40} fill="hsl(0, 0%, 40%)"/></button>
-      {modifiedSlides.map((card) => {
-        return <Card key={card.id} number={card.number} image={card.src}/>;
-      })}
-      <button onClick={handleShiftRight}><FaChevronRight  size={40} fill="hsl(0, 0%, 40%)"/></button>
-    </div>
+    <Slider />
   );
 }
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ const Card = ({number, image}) => {
   return (
     <div className='flex items-center justify-center w-96 h-52 bg-slate-400 my-16 overflow-hidden relative'>
         <span className='font-bold text-7xl text-yellow-100 absolute'>{number}</span>
-        <img src={image} alt="Image" className='w-full '/>
+        <img src={image} alt="content" className='w-full '/>
     </div>
   )
 }
